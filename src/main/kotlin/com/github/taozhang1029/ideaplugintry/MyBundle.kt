4b13ca985f3edfa1eb742a3b80b01bb0ec3fdfd1
GitHub Action <action@github.com>
2022-08-18 12:04:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/taozhang1029/ideaplugintry/MyBundle.kt
+++ src/main/kotlin/com/github/taozhang1029/ideaplugintry/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.taozhang1029.ideaplugintry
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/taozhang1029/ideaplugintry/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/taozhang1029/ideaplugintry/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.taozhang1029.ideaplugintry.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.taozhang1029.ideaplugintry.MyBundle
 
 class MyApplicationService {
 
